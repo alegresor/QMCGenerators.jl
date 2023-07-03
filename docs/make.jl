259dcc485de6f11ff14f133db7a3f7633f3e29be
alegresor <agsorokin3@gmail.com>
2023-07-03 07:53:42 -0600

    no longer convert reamde to docs index

--- docs/make.jl
+++ docs/make.jl
@@ -2,12 +2,6 @@
 
 using Documenter, QMCGenerators
 
-readme = read(joinpath(@__DIR__(),"../README.md"),String)
-readme = replace(readme,"https://alegresor.github.io/QMCGenerators.jl/stable/tutorial"=>"@ref")
-readme = replace(readme,"https://alegresor.github.io/QMCGenerators.jl/dev"=>".")
-readme = replace(readme,"./docs/src"=>".")
-write(joinpath(@__DIR__(),"./src/index.md"),readme)
-
 assetsdir = joinpath(@__DIR__,"src/assets")
 if ~isdir(assetsdir) mkdir(assetsdir) end
 
