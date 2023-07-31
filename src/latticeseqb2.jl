1990a13b679704bd795c18af2645c5eda247308b
alegresor <agsorokin3@gmail.com>
2023-07-31 08:42:11 -0600

    optimized lattice base

--- src/latticeseqb2.jl
+++ src/latticeseqb2.jl
@@ -1,11 +1,10 @@
 mutable struct LatticeSeqB2
     const name::String
     const s::Int64 # dimension 
-    const z::Union{Vector{UInt64},Vector{UInt128},Vector{BigInt}}
+    const z::Vector{UInt64}
     const m::Int64 # 2^m points supported
     const n::Int64 # 2^m
     const scale::Union{BigFloat,Float64} # 2^(-m)
-    const TInt::DataType
     k::Int64 # index in the sequence
 end
 
@@ -14,9 +13,9 @@ function LatticeSeqB2(s::Int64,z::Vector{BigInt},m::Int64)
     n = 2^m 
     scale = m>53 ? BigFloat(2)^(-m) : Float64(2)^(-m)
     t = maximum(ndigits.(z[1:s],base=2))
-    TInt = nothing; if t<=64 TInt = UInt64 elseif t<=128 TInt = UInt128 else TInt = BigInt end
-    z = convert.(TInt,z[1:s])
-    LatticeSeqB2("Lattice Seq B2",s,z,m,n,scale,TInt,-1)
+    @assert t<=64
+    z = convert.(UInt64,z[1:s])
+    LatticeSeqB2("Lattice Seq B2",s,z,m,n,scale,-1)
 end
 
 LatticeSeqB2(s::Int64,path::String,m::Int64) = LatticeSeqB2(s,readdlm(download(joinpath("https://bitbucket.org/dnuyens/qmc-generators/raw/cb0f2fb10fa9c9f2665e41419097781b611daa1e/LATSEQ/",path)),BigInt)[:,1],m)
@@ -28,26 +27,43 @@ function Reset!(seq::LatticeSeqB2)
     return
 end
 
-function NextLow(xb::Matrix{Float64},k::Int64,n::Int64,z::Union{Vector{UInt64},Vector{UInt128},Vector{BigInt}})
-    nvec = range(k+1,k+n)
-    p2s = 2 .^ ceil.(log2.(nvec.+1))
-    fracs = (2 .* (p2s.-nvec) .- 1) ./ p2s
-    xb[:,:] .= convert.(Float64,(fracs*z').%1)
+function NextLow(s::Int64,xb::Matrix{Float64},k::Int64,n::Int64,z::Vector{UInt64})
+    idx::Int64,p2::Int64,frac::Float64 = 0,0,0
+    for i=1:n 
+        idx = k+i 
+        p2 = 2^ceil(Int64,log2(idx+1))
+        frac = (2*(p2-idx)-1)/p2
+        for j=1:s
+            xb[i,j] = convert(Float64,(frac*z[j])%1)
+        end 
+    end 
 end 
 
 function Next(seq::LatticeSeqB2,n::Int64)
     (seq.k+n)>=seq.n && throw(DomainError(n,"Generating $n more points will exceed the maximum supported points $(seq.n)"))
     xb::Matrix{Float64} = zeros(Float64,n,seq.s)
-    NextLow(xb,seq.k,n,seq.z)
+    NextLow(seq.s,xb,seq.k,n,seq.z)
     seq.k += n
     xb
 end 
+
+function FirstLinearLow(xb::Matrix{Float64},s::Int64,n::Int64,z::Vector{UInt64})
+    frac::Float64 = 0
+    for i=1:n
+        frac = (i-1)/n
+        for j=1:s
+            xb[i,j] = convert(Float64,(frac*z[j])%1)
+        end 
+    end
+end
 
 function FirstLinear(seq::LatticeSeqB2,m::Int64)
     @assert seq.k == -1 
     n = 2^m
     (seq.k+n)>=seq.n && throw(DomainError(n,"Generating $n more points will exceed the maximum supported points $(seq.n)"))
-    convert.(Float64,(range(0,n-1)*(seq.z'./n)).%1)
+    xb::Matrix{Float64} = zeros(Float64,n,seq.s)
+    FirstLinearLow(xb,seq.s,n,seq.z)
+    xb
 end 
 
 struct RandomShift
