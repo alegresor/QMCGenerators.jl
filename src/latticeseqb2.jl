2ba11da41e25ff279c6acdc95898b9ad15ace3b7
alegresor <agsorokin3@gmail.com>
2023-08-04 12:34:34 -0600

    update lattice orderings

--- src/latticeseqb2.jl
+++ src/latticeseqb2.jl
@@ -4,18 +4,20 @@ mutable struct LatticeSeqB2
     const z::Vector{UInt64}
     const m::Int64 # 2^m points supported
     const n::Int64 # 2^m
-    const scale::Union{BigFloat,Float64} # 2^(-m)
+    const scale::Float64 # 2^(-m)
     k::Int64 # index in the sequence
+    v::UInt64 # current bitreversed binary
 end
 
 function LatticeSeqB2(s::Int64,z::Vector{BigInt},m::Int64)
     @assert m > 0 && s <= length(z)
+    @assert m<=53
     n = 2^m 
-    scale = m>53 ? BigFloat(2)^(-m) : Float64(2)^(-m)
+    scale = Float64(2)^(-m)
     t = maximum(ndigits.(z[1:s],base=2))
     @assert t<=64
     z = convert.(UInt64,z[1:s])
-    LatticeSeqB2("Lattice Seq B2",s,z,m,n,scale,-1)
+    LatticeSeqB2("Lattice Seq B2",s,z,m,n,scale,-1,UInt64(0))
 end
 
 LatticeSeqB2(s::Int64,path::String,m::Int64) = LatticeSeqB2(s,readdlm(download(joinpath("https://bitbucket.org/dnuyens/qmc-generators/raw/cb0f2fb10fa9c9f2665e41419097781b611daa1e/LATSEQ/",path)),BigInt)[:,1],m)
@@ -24,45 +26,49 @@ LatticeSeqB2(s::Int64) = LatticeSeqB2(s,DEFAULT_LATTICESEQB2_GVECTOR,20)
 
 function Reset!(seq::LatticeSeqB2)
     seq.k = -1
+    seq.v = 0
     return
 end
 
-function NextLow(s::Int64,xb::Matrix{Float64},k::Int64,n::Int64,z::Vector{UInt64})
-    idx::Int64,p2::Int64,frac::Float64 = 0,0,0
-    for i=1:n 
-        idx = k+i 
-        p2 = 2^ceil(Int64,log2(idx+1))
-        frac = (2*(p2-idx)-1)/p2
-        for j=1:s
-            xb[i,j] = convert(Float64,(frac*z[j])%1)
-        end 
-    end 
+function NextLow(i1::UInt64,v::UInt64,xb::Matrix{Float64},s::Int64,n::Int64,k::Int64,z::Vector{UInt64},scale::Float64,m::Int64)
+    b::Int64=0; frac::Float64=0
+    for i::UInt64=i1:n
+        k += 1
+        b = 0; while ~Bool((k>>b)&1) b+= 1 end
+        v ⊻= UInt64(1)<<(m-1-b)
+        frac = scale*v
+        for j=1:s xb[i,j] = convert(Float64,(frac*z[j])%1) end 
+    end
+    v
 end 
 
 function Next(seq::LatticeSeqB2,n::Int64)
     (seq.k+n)>=seq.n && throw(DomainError(n,"Generating $n more points will exceed the maximum supported points $(seq.n)"))
     xb::Matrix{Float64} = zeros(Float64,n,seq.s)
-    NextLow(seq.s,xb,seq.k,n,seq.z)
+    seq.v = NextLow(seq.k==-1 ? UInt64(2) : UInt64(1),seq.v,xb,seq.s,n,seq.k==-1 ? 0 : seq.k,seq.z,seq.scale,seq.m)
     seq.k += n
     xb
 end 
 
-function FirstLinearLow(xb::Matrix{Float64},s::Int64,n::Int64,z::Vector{UInt64})
-    frac::Float64 = 0
-    for i=1:n
-        frac = (i-1)/n
-        for j=1:s
-            xb[i,j] = convert(Float64,(frac*z[j])%1)
-        end 
+function FirstLinearLow(xb::Matrix{Float64},s::Int64,n::Int64,k::Int64,z::Vector{UInt64},scale::Float64,m::Int64)
+    b::Int64=0; v::UInt64=0; frac::Float64=0
+    for i::UInt64=1:n-1
+        igc = (i⊻(i>>1))+1
+        k += 1
+        b = 0; while ~Bool((k>>b)&1) b+= 1 end
+        v ⊻= UInt64(1)<<(m-1-b)
+        frac = scale*v
+        for j=1:s xb[igc,j] = convert(Float64,(frac*z[j])%1) end 
     end
 end
 
-function FirstLinear(seq::LatticeSeqB2,m::Int64)
+function FirstLinear(seq::LatticeSeqB2,n::Int64)
     @assert seq.k == -1 
-    n = 2^m
+    @assert ispow2(n)
     (seq.k+n)>=seq.n && throw(DomainError(n,"Generating $n more points will exceed the maximum supported points $(seq.n)"))
     xb::Matrix{Float64} = zeros(Float64,n,seq.s)
-    FirstLinearLow(xb,seq.s,n,seq.z)
+    FirstLinearLow(xb,seq.s,n,seq.k==-1 ? 0 : seq.k,seq.z,seq.scale,seq.m)
+    Reset!(seq)
     xb
 end 
 
@@ -98,9 +104,8 @@ function NextR(rls::RandomShift,n::Int64)
     xr
 end 
 
-function FirstRLinear(rls::RandomShift,m::Int64)
-    n = 2^m
-    xu = FirstLinear(rls.seq,m)
+function FirstRLinear(rls::RandomShift,n::Int64)
+    xu = FirstLinear(rls.seq,n)
     xr = [zeros(Float64,n,rls.seq.s) for k=1:rls.r]
     NextRLowRandomShift(rls.seq.s,rls.r,n,xu,xr,rls.rshifts)
     xr
